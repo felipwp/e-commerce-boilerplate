49db7ee3410ede3bdf10feaf926ed616505d571f
Felipe Pereira <felipwp@hotmail.com>
2021-01-22 20:55:49 -0300

    Crud com GraphQL e MikroORM

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
+import "reflect-metadata"
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Product } from "./entities/product";
+import express from "express";
+import { ApolloServer } from 'apollo-server-express';
 import microConfig from './mikro-orm.config';
+import { buildSchema } from "type-graphql";
+import { ProductResolver } from "./resolvers/product";
 
 // função usada para não precisar setar o arquivo todo como assíncrono
 const main = async () => {
-
     // conecta na db
     const orm = await MikroORM.init(microConfig);
 
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [ProductResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    apolloServer.applyMiddleware({app});
+
+    app.listen(8888, () => {
+        console.log("Server listening port 8888")
+    });
     // migrations
     //
     // também é possível rodar o comando npx mikro-orm migration:up
---
+++ src/resolvers/product.ts
@@ -0,0 +1,66 @@
+import { Product } from "../entities/product";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
+import { MyContext } from "../types";
+
+@Resolver()
+export class ProductResolver {
+
+    // lista todos os produtos
+    // Ctx é o context, onde é passada a instância da DB
+    @Query(() => [Product])
+    getAllProducts(@Ctx() { em }: MyContext
+        // A query irá retornar uma Promise, que contém um Array de <Product>
+    ): Promise<Product[] | null> {
+        return em.find(Product, {});
+    }
+
+    // lista apenas um produto
+    @Query(() => Product, { nullable: true })
+    getProduct(
+        @Arg('id', () => Int) id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<Product | null> {
+        return em.findOne(Product, { id });
+    }
+
+
+    // Cria um produto
+    @Mutation(() => Product)
+    async createProduct(
+        // O argumento necessita de duas coisas
+        // a tipagem para o TypeGraphQL (() => String)
+        // e a tipagem Typescript (name: string)
+        @Arg('name', () => String) name: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Product> {
+        const product = em.create(Product, { name });
+        await em.persistAndFlush(product);
+        return product;
+    }
+
+    // Atualiza um produto
+    @Mutation(() => Product, { nullable: true })
+    async updateProduct(
+        @Arg('id', () => Int) id: number,
+        // toda vez que um campo possa ser editado ou não, deve-se adicionar
+        // { nullable: true } ao argumento
+        @Arg('name', () => String, { nullable: true }) name: string,
+        @Ctx() { em }: MyContext
+
+        // caso a função possa retornar null,
+        // ele deve ser adicionado abaixo 
+        // Promise<Product | null>, indica que pode voltar uma Promise de <Product> ou null
+    ): Promise<Product | null> {
+        const product = await em.findOne(Product, { id });
+
+        // caso não exista nenhum produto com aquele id
+        if (!product) return null;
+
+        if (typeof name !== 'undefined') {
+            product.name = name;
+            await em.persistAndFlush(product);
+        }
+        return product;
+    }
+
+}
